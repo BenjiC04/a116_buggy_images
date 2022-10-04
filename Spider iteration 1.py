11b078793caca0ee44fddeff1a3f64851b352692
Ben Clawson <113128420+BenjiC04@users.noreply.github.com>
2022-10-03 20:11:14 -0700

    Add files via upload

---
+++ Spider iteration 1.py
@@ -0,0 +1,29 @@
+#   a116_buggy_image.py
+import turtle as trtl
+# instead of a descriptive name of the turtle such as painter,
+# a less useful variable name spider is used
+spider = trtl.Turtle()
+
+#create spider body
+spider.pensize(40)
+spider.circle(20)
+
+#configure spider legs
+num_legs = 8
+leg_size = 90
+z = 380 / num_legs
+print("z1=", z)
+spider.pensize(5)
+n = 0
+
+#draw legs
+while (n < num_legs):
+  spider.goto(0,0)
+  spider.setheading(z*n)
+  spider.forward(leg_size)
+  n = n + 1
+  print ("z*n=", z*n)
+spider.hideturtle()
+
+wn = trtl.Screen()
+wn.mainloop()
